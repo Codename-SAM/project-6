8a2057430f4cc92fd8a73753b57ab9989ca4edde
Matt <matthewdigiacomo@yahoo.com>
2014-11-20 12:40:12 -0500

    Update courseList.java

--- courseList.java
+++ courseList.java
@@ -1,8 +1,22 @@
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-
-public class courseList
+public class CourseList
 {
+	private String[] names = {"MAT107", "MAT151", "ESS111", "ESS101", "WRT200", "WRT220", "HIS100", "HIS200", "CSC141", "CSC142"};
+	private String[] instructor_usr = {"PROF1", "PROF2", "PROF3", "PROF4", "PROF5", "PROF6", "PROF7", "PROF8", "PROF9", "PROF10"};
+	private int[] number_of_enroll;
+	private String[][] student_list;
 
+	public CourseList()
+	{
+
+	}
+
+	public String[] getClasses()
+	{
+		return names;
+	}
+
+	public void assignStudent(String[] classes, String[][] roster)
+	{
+
+	}
 }
