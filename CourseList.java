a1b928b8bf3c987ec41d4f55cbedf278601a631d
Matt <matthewdigiacomo@yahoo.com>
2014-11-20 12:41:51 -0500

    Update and rename courseList.java to CourseList.java

---
+++ CourseList.java
@@ -0,0 +1,22 @@
+public class CourseList
+{
+	private String[] names = {"MAT107", "ESS101", "WRT200", "HIS100", "CSC142"};
+	private String[] instructor_usr = {"PROF1", "PROF2", "PROF3", "PROF4", "PROF5"};
+	private int[] number_of_enroll;
+	private String[][] student_list;
+
+	public CourseList()
+	{
+
+	}
+
+	public String[] getClasses()
+	{
+		return names;
+	}
+
+	public void assignStudent(String[] classes, String[][] roster)
+	{
+
+	}
+}
