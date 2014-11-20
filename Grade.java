dd6daa29dcb86294331f95d440922052f300047f
Matt <matthewdigiacomo@yahoo.com>
2014-11-19 22:41:47 -0500

    Update Grade.java

--- Grade.java
+++ Grade.java
@@ -4,35 +4,38 @@ import javax.swing.*;
 
 public class Grade extends JFrame implements ActionListener
 {
-	//Login-Requirements-------------
+	Container c = getContentPane();
+
+	//Program-Login-Requirements-----
 	private JTextField userField;
 	private JPasswordField passField;
 	private JButton loginButton;
 	private JLabel label;
 	//-------------------------------
 
+	//Java-Classes-------------------
+	CourseList cl = new CourseList();
+
+	//Admin-Content------
+
 	public Grade()
 	{
 		super("Grade");
-		Container c = getContentPane();
 		setLayout(new FlowLayout());
 
+		userField = new JTextField("admin", 10);
+		passField = new JPasswordField("", 10);
+		loginButton = new JButton("Login");
+		label = new JLabel("Please enter your administrator login information.");
 
-			//Login-Initialization-----------------------------
-			userField = new JTextField("admin", 10);
-			passField = new JPasswordField("password", 15);
-			loginButton = new JButton("Login");
-			label = new JLabel("Please enter your login information.");
+		c.add(userField);
+		c.add(passField);
+		c.add(loginButton);
+		c.add(label);
 
-			c.add(userField);
-			c.add(passField);
-			c.add(loginButton);
-			c.add(label);
-
-			userField.addActionListener(this);
-			passField.addActionListener(this);
-			loginButton.addActionListener(this);
-			//--------------------------------------------------
+		userField.addActionListener(this);
+		passField.addActionListener(this);
+		loginButton.addActionListener(this);
 
 		setVisible(true);
 		setResizable(false);
@@ -47,21 +50,25 @@ public class Grade extends JFrame implements ActionListener
 
 		if(obj == loginButton)
 		{
-			if(name.equals("admin") && pass.equals("PASS13"))
-			{
-				label.setText("Login Successful!"); /* Welcome + name of person );*/
-				setSize(800, 600);
-				setResizable(true);
-			}
+			login(name, pass);
+		}
+	}
 
-			else
-			{
-				label.setText("Please enter a valid username/password combination.");
-				setSize(400, 100);
-				setResizable(false);
-			}
+	public void login(String name, String pass)
+	{
+		if(name.equals("admin") && pass.equals("PASS13"))
+		{
+			label.setText("Login Successful!");
+			setSize(800, 600);
+			setResizable(true);
 		}
 
+		else
+		{
+			label.setText("Please enter a valid username/password combination.");
+			setSize(400, 100);
+			setResizable(false);
+		}
 	}
 
 	public static void main(String[]args)
