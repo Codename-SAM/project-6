7054b48c5da368f1ac8e81c051c3750dbe27a055
Matt <matthewdigiacomo@yahoo.com>
2014-11-20 12:40:34 -0500

    Update instructorList.java

--- instructorList.java
+++ instructorList.java
@@ -1,1 +1,21 @@
-//InstrustorList
+public class Instructor
+{
+	private String username, password;
+
+
+	public Instructor()
+	{
+
+	}
+
+	public int login(String name, String pass)
+	{
+		if(name.equals("admin") && pass.equals("PASS13"))
+		{
+			return 1;
+		}
+
+		else
+			return 0;
+	}
+}
