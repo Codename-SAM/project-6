69cabdf24ab01a5b0fc90110b28eba03ba8d4f71
Matt <matthewdigiacomo@yahoo.com>
2014-11-24 22:52:28 -0500

    Update CourseList.java

--- CourseList.java
+++ CourseList.java
@@ -2,10 +2,11 @@ import javax.swing.*;
 
 public class CourseList
 {
-	private int namesNum, instructorNum;
+	private int namesNum, instructorNum, studentNum;
 
 	private String[] names;
 	private String[] instructor_usr;
+	private String[] students;
 	private int[] number_of_enroll; //number of studens enrolled
 	private String[][] student_list; //A 2-D array of all the rosters with the respective students in each class, this comes last
 
@@ -21,16 +22,18 @@ public class CourseList
 			namesNum = Integer.parseInt(num);
 			num = JOptionPane.showInputDialog("How many professors should be initialized?");
 			instructorNum = Integer.parseInt(num);
+			num = JOptionPane.showInputDialog("How many students should be initialized");
+			studentNum = Integer.parseInt(num);
 		}
 
-		public void setCourses()
-		{
-			int count = 0;
-			names[count] = JOptionPane.showInputDialog("Enter the Name of Course " + (count + 1) + ", with the course number and title, like CSC142 Programming in Java");
-
-			if(count < names.length)
+			public void setCourses()
 			{
-				setCourses();
+				System.out.println("Called!");
+
+				for(int a=0; a < names.length; a++)
+				{
+					System.out.println("accessed for loop");
+					names[a] = JOptionPane.showInputDialog("Set the name of course " + a);
+				}
 			}
-		}
 }
