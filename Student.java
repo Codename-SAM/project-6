53f61b4a42d5d16b8793e8463928703dca230115
RedentorSC <redentorsc@yahoo.com>
2014-11-25 12:59:29 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -2,3 +2,21 @@ import java.awt.*;
 import javax.swing.*;
 import javax.swing.event.*;
 
+public class Student extends JFrame(){
+  private String userid;
+  private String password;
+  
+  public Student(){
+    
+    
+    
+  }
+  
+  void setName(String userid){
+    this.userid = userid;
+  }
+  
+  void setPass(String newPass){
+    this.password = newPass;
+  }
+}
