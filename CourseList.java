913ffe540657336cb80642ce64d6d824ca980ff5
Matt <matthewdigiacomo@yahoo.com>
2014-11-24 23:30:53 -0500

    Update CourseList.java

--- CourseList.java
+++ CourseList.java
@@ -3,37 +3,53 @@ import javax.swing.*;
 public class CourseList
 {
 	private int namesNum, instructorNum, studentNum;
-
-	private String[] names;
-	private String[] instructor_usr;
-	private String[] students;
-	private int[] number_of_enroll; //number of studens enrolled
+	private String[] names;          //number of classes
+	private String[] instructor_usr; //number of professors
+	private String[] students;       //number of students in general
+	private int[] number_of_enroll;  //number of studens enrolled
 	private String[][] student_list; //A 2-D array of all the rosters with the respective students in each class, this comes last
 
 	public CourseList()
 	{
-		names = new String[namesNum];
-		instructor_usr = new String[instructorNum];
+
 	}
 
 		public void setLength()
 		{
 			String num = JOptionPane.showInputDialog("How many classes should be initialzied?");
 			namesNum = Integer.parseInt(num);
+			names = new String[namesNum];
 			num = JOptionPane.showInputDialog("How many professors should be initialized?");
 			instructorNum = Integer.parseInt(num);
+			instructor_usr = new String[instructorNum];
 			num = JOptionPane.showInputDialog("How many students should be initialized");
 			studentNum = Integer.parseInt(num);
+			students = new String[studentNum];
 		}
 
-			public void setCourses()
+			public String[] getCourses()
 			{
-				System.out.println("Called!");
-
-				for(int a=0; a < names.length; a++)
-				{
-					System.out.println("accessed for loop");
-					names[a] = JOptionPane.showInputDialog("Set the name of course " + a);
-				}
+				return names;
 			}
+
+			public String[] getStudents()
+			{
+				return students;
+			}
+
+				public void setCourses()
+				{
+					for(int a=0; a < names.length; a++)
+					{
+						names[a] = JOptionPane.showInputDialog("Set the name of course " + a);
+					}
+				}
+
+				public void setStudents()
+				{
+					for(int a=0; a < students.length; a++)
+					{
+						students[a] = JOptionPane.showInputDialog("Set the name of student " + a);
+					}
+				}
 }
