675fa556752703930a273f0d03577de58389bba1
Matt <matthewdigiacomo@yahoo.com>
2014-11-24 22:52:01 -0500

    Update Grade.java

--- Grade.java
+++ Grade.java
@@ -6,8 +6,18 @@ public class Grade extends JFrame implements ActionListener
 {
 	private JTextField username;
 	private JPasswordField password;
-	private JLabel label;
 	private JButton login;
+	private JLabel label;
+
+	private JLabel label1;
+	private JLabel label2;
+	private JLabel label3;
+
+	private JTextField text1;
+	private JButton button1;
+	private JButton button2;
+	private JButton button3;
+
 	private Container c;
 	private CourseList cl;
 
@@ -24,6 +34,8 @@ public class Grade extends JFrame implements ActionListener
 		label = new JLabel("Please enter the username/password combination.");
 		login = new JButton("Login");
 
+		button1 = new JButton("Designate people");
+
 		c.add(username);
 		c.add(password);
 		c.add(label);
@@ -32,6 +44,7 @@ public class Grade extends JFrame implements ActionListener
 		username.addActionListener(this);
 		password.addActionListener(this);
 		login.addActionListener(this);
+		button1.addActionListener(this);
 
 		setSize(400, 100);
 		setResizable(false);
@@ -50,8 +63,7 @@ public class Grade extends JFrame implements ActionListener
 			if(user.equals("admin") && pass.equals("PASS13"))
 			{
 				cl.setLength();
-				cl.setCourses();
-				setVisible(false);
+				setMainFrame();
 			}
 
 			else
@@ -59,6 +71,19 @@ public class Grade extends JFrame implements ActionListener
 				label.setText("Please try again.");
 			}
 		}
+
+		if(obj == button1)
+		{
+			cl.setCourses();
+		}
+	}
+
+	public void setMainFrame()
+	{
+		c.removeAll();
+		c.add(button1);
+		setSize(600, 480);
+		repaint();
 	}
 
 	public static void main(String[] args)
