2360459660bf1700456571df769fa3bba5403ddc
Matt <matthewdigiacomo@yahoo.com>
2014-12-02 01:23:10 -0500

    Update CourseList.java

--- CourseList.java
+++ CourseList.java
@@ -2,7 +2,8 @@ import javax.swing.*;
 
 public class CourseList
 {
-	private int namesNum, instructorNum, studentNum;
+	private int namesNum = 0, instructorNum = 0, studentNum = 0;
+
 	private String[] names;          //number of classes
 	private String[] instructor_usr; //number of professors
 	private String[] students;       //number of students in general
@@ -14,34 +15,70 @@ public class CourseList
 
 	}
 
-		public void setLength()
-		{
-			String num = JOptionPane.showInputDialog("How many classes should be initialzied?");
-			namesNum = Integer.parseInt(num);
-			names = new String[namesNum];
-			num = JOptionPane.showInputDialog("How many professors should be initialized?");
-			instructorNum = Integer.parseInt(num);
-			instructor_usr = new String[instructorNum];
-			num = JOptionPane.showInputDialog("How many students should be initialized");
-			studentNum = Integer.parseInt(num);
-			students = new String[studentNum];
-		}
+			public void setLength()
+			{
+				String num;
+
+				while(namesNum < 1)
+				{
+					num = JOptionPane.showInputDialog("How many classes should be initialzied?");
+					namesNum = Integer.parseInt(num);
+				}
+
+				names = new String[namesNum];
+
+				while(instructorNum < 2)
+				{
+					num = JOptionPane.showInputDialog("How many professors should be initialized?");
+					instructorNum = Integer.parseInt(num);
+				}
+
+				instructor_usr = new String[instructorNum];
+
+				while(studentNum < 5)
+				{
+					num = JOptionPane.showInputDialog("How many students should be initialized");
+					studentNum = Integer.parseInt(num);
+				}
+
+				students = new String[studentNum];
+			}
 
 			public String[] getCourses()
 			{
 				return names;
 			}
 
+				public void updateCourses(String[] temp)
+				{
+					names = temp;
+				}
+
 			public String[] getStudents()
 			{
 				return students;
 			}
 
+				public void updateStudents(String[] temp)
+				{
+					students = temp;
+				}
+
+			public String[] getInstructors()
+			{
+				return instructor_usr;
+			}
+
+				public void updateInstructors(String[] temp)
+				{
+					instructor_usr = temp;
+				}
+
 				public void setCourses()
 				{
 					for(int a=0; a < names.length; a++)
 					{
-						names[a] = JOptionPane.showInputDialog("Set the name of course " + a);
+						names[a] = JOptionPane.showInputDialog("Set the name of course " + (a + 1));
 					}
 				}
 
@@ -49,7 +86,15 @@ public class CourseList
 				{
 					for(int a=0; a < students.length; a++)
 					{
-						students[a] = JOptionPane.showInputDialog("Set the name of student " + a);
+						students[a] = JOptionPane.showInputDialog("Set the name of student " + (a + 1));
+					}
+				}
+
+				public void setInstructors()
+				{
+					for(int a=0; a < instructor_usr.length; a++)
+					{
+						instructor_usr[a] = JOptionPane.showInputDialog("Set the name of instructor " + (a + 1));
 					}
 				}
 }
