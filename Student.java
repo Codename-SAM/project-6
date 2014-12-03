bcdc4ef810976181134dc49ae4acf2b5a747c1fc
RedentorSC <redentorsc@yahoo.com>
2014-12-02 22:31:40 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,11 +1,13 @@
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.event.*;
+import java.util.ArrayList
 
 public class Student(){
   private String userid;
   private String password;
-  private String [] courses = new String[5];
+  private String [] courses = new String[]{"Courses: "};
+  private double [][] hwGrade = new double [1][1];
   
   public Student(){
     
@@ -19,6 +21,18 @@ public class Student(){
   
   public void setPass(String newPass){
     this.password = newPass;
+  }
+  
+  public void updateCourses(String newCourse){
+    String [] updatedCourses = new String[courses.length + 1]; //instantiate the copy array
+    int i = 0;
+    for(; i<courses.length; i++){
+      updatedCourses[i] = courses[i]; //copy the elements
+      updatedCourses[courses.length] = [newCourse]; //the last index of the new array becomes the latest course
+      RETURN THIS SUCH THAT THE COURSES ATTRIBUTE = THE UPDATED COURSES
+    }
+    
+    
   }
   
   
